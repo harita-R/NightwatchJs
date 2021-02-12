4a161f3544dfaf2613b75e606e0ecf6c599caf77
Harita Ravindranath <harrysama01@outlook.com>
2021-02-12 23:21:05 +0530

    reports

---
+++ nightwatch.conf.js
@@ -0,0 +1,21 @@
+const chromedriver = require('chromedriver');
+
+module.exports = {
+  src_folders : ["tests"],
+  output_folder : "reports",
+
+  webdriver: {
+    start_process: true,
+    server_path: "node_modules/chromedriver/lib/chromedriver/chromedriver.exe",
+    port: 4444,
+    host: "localhost"
+  },
+
+  test_settings: {
+    default: {    
+      desiredCapabilities: {
+        browserName: 'chrome'
+      }
+    }
+  }
+};
